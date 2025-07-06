67bf0eefe325f499a6576a023ae1cb574d673aa6
Moeen Mahmud <write2fahim007@gmail.com>
2025-07-06 19:35:33 +0600

    Add LFS tracked files

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    [Header("Effects")]
+    public GameObject particleEffectPrefab; // Assign particle system prefab in Inspector
+
+    public AudioClip collectSound;
+
+    [Header("Motion Settings")]
+    public float rotationSpeed = 100f; // Rotation speed in degrees per second
+    public float bobbingAmount = 0.5f; // Amplitude of bobbing motion
+    public float bobbingSpeed = 2f; // Speed of bobbing motion
+
+    private Vector3 startPosition;
+    private float timer; 
+
+    void Start()
+    {
+        // Remember the original position of the GameObject
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Rotate the object around its up axis
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+
+        // Create a bobbing motion up and down
+        timer += Time.deltaTime * bobbingSpeed;
+        float newY = startPosition.y + Mathf.Sin(timer) * bobbingAmount;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        
+        // If it's the last piece, go to the end screen
+        if (timer >= 1f)
+        {
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the colliding object has the "Player" tag
+        if (!other.CompareTag("Player")) return;
+        if (particleEffectPrefab == null) return;
+        
+        Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
+        AudioSource.PlayClipAtPoint(collectSound, transform.position);
+        // Destroy the star
+        Destroy(gameObject);
+    }
+}
